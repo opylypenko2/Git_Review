a21ea6de55bdc40852770d4e3e2d62f3c199ea99
Olena Pylypenko <olena.pylypenko@gmail.com>
2022-08-23 11:55:02 -0400

    added the package in Loop.java; added loop body

--- src/first/Loop.java
+++ src/first/Loop.java
@@ -1,3 +1,9 @@
+package first;
+
 public class Loop {
-  public static void main (String [] args) {
-    
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+        }
+    }
+}
